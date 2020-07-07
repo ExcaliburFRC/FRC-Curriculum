a1c93a712ede83be4962d75615f71a8f1da456bf
NirLevanon <63192579+NirLevanon@users.noreply.github.com>
2020-07-07 10:23:03 +0300

    Update Ex1.java

--- 09/src/analysis/a/Ex1.java
+++ 09/src/analysis/a/Ex1.java
@@ -2,7 +2,12 @@ package analysis.a;
 
 public class Ex1 {
   /**
-   * Checks if two arrays are disjoint (no common items)
+   * Checks if two arrays share at least 1 common value somewhere within them (the array are called "disjoint")
+   *  true:      a       b       false:            a     b   
+   *             1       2                         1     2
+   *             3       4                         3     5    // will exit after 5 iterations
+   *             5       6                         5     6
+   *
    */
   public static boolean disjoint(int[] a, int[] b) {
     for (int i = 0; i < a.length; i++) {
