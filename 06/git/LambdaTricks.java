0e8ae3c9fb579449b68093d5dd2e4786cb7f074f
Starlight220 <53231611+Starlight220@users.noreply.github.com>
2020-10-11 11:50:51 +0300

    [06] Add LambdaTricks.java (#27)
    From lesson 06
    * another usage example
    
    * add repeat function
    
    * add docs

--- 06/git/LambdaTricks.java
+++ 06/git/LambdaTricks.java
@@ -1,12 +1,30 @@
 package git;
 
-import java.util.function.Consumer;
-import java.util.function.Supplier;
+import java.util.Scanner;
+import java.util.function.*;
 
+/**
+ * All kinds of tricks with lambdas
+ */
 public class LambdaTricks {
   public static <T> Runnable combine(Supplier<T> supplier, Consumer<T> consumer) {
     return () -> {
       consumer.accept(supplier.get());
     };
+  }
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    Runnable run2 = combine(() -> in.nextLine().toUpperCase(),
+            System.out::println);
+    for (int i = 0; i < 8; i++) {
+      run2.run();
+    }
+  }
+
+  public static void repeat(int i, IntConsumer action) {
+    for (int j = 0; j < i; j++) {
+      action.accept(i);
+    }
   }
 }
