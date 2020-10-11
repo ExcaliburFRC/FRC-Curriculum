ff4e61bbb35a5f91dddc9396588a0b7a153d19bd
Starlight220 <yotamshlomi@gmail.com>
2020-10-11 11:20:06 +0300

    another usage example

--- 06/git/LambdaTricks.java
+++ 06/git/LambdaTricks.java
@@ -1,5 +1,6 @@
 package git;
 
+import java.util.Scanner;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -8,5 +9,14 @@ public class LambdaTricks {
     return () -> {
       consumer.accept(supplier.get());
     };
+  }
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    Runnable run2 = combine(() -> in.nextLine().toUpperCase(),
+            System.out::println);
+    for (int i = 0; i < 8; i++) {
+      run2.run();
+    }
   }
 }
