85b167af47002ea663ce7fb6295db3b5a1c2ffc6
Starlight220 <53231611+Starlight220@users.noreply.github.com>
2020-10-14 14:08:04 +0300

    add Message.java

---
+++ 09/classes/Message.java
@@ -0,0 +1,33 @@
+package classes;
+
+public class Message {
+  private String message; // a private object property/field
+  
+  /**
+   * Create a new Message object with the given message String.
+   */
+  public Message(String message) {
+    this.message = message;
+  }
+  
+  /**
+   * Print the message to an output.
+   */
+  public void printMessage(Consumer<String> output) {
+    output.accept(message);
+  }
+  
+  /**
+   * Print the message to {@code System.out }
+   */
+  public void printMessage() {
+    this.printMessage(System.out::println);
+  }
+  
+  /**
+   * Changes the message stored.
+   */
+  public void setMessage(String newMessage) {
+    this.message = newMessage;
+  }
+}
