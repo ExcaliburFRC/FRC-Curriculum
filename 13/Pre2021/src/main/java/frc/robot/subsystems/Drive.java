167a3339e016ed572c55eb6f27230f4d59e00990
elon698 <73128447+elon698@users.noreply.github.com>
2020-12-26 22:23:28 +0200

    completing the driving system

--- 13/Pre2021/src/main/java/frc/robot/subsystems/Drive.java
+++ 13/Pre2021/src/main/java/frc/robot/subsystems/Drive.java
@@ -2,21 +2,53 @@ package frc.robot.subsystems;
 
 import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drive extends SubsystemBase {
 
-    private CANSparkMax rightLeader ;
-    private CANSparkMax rightFollower ;
-    private CANSparkMax leftLeader ;
-    private CANSparkMax leftFollower ;
-    private Encoder leader ;
-    private Encoder follower ;
-    private AHRS gyro ;
+  private CANSparkMax rightLeader;
+  private CANSparkMax rightFollower;
+  private CANSparkMax leftLeader;
+  private CANSparkMax leftFollower;
+  private Encoder leftEncoder;
+  private Encoder rightEncoder;
+  private AHRS gyro;
+  private DifferentialDrive drive;
 
+  public Drive() {
+    rightLeader = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
+    leftLeader = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
+    leftFollower = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
+    rightFollower = new CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless);
+    rightFollower.follow(rightLeader);
+    leftFollower.follow(leftLeader);
+    leftEncoder = new Encoder(0, 1);
+    rightEncoder = new Encoder(2, 3);
+    drive = new DifferentialDrive(leftLeader, rightLeader);
+    gyro = new AHRS();
+  }
 
-    public Drive() {
-//        rightLeader = new CANSparkMax()
-    }
+  public void tankDrive(double right, double left) {
+    rightLeader.set(right);
+    leftLeader.set(left);
+  }
+
+  public double getLeftEncoder() {
+    return leftEncoder.getDistance();
+  }
+
+  public double getRightEncoder() {
+    return rightEncoder.getDistance();
+  }
+
+  public double getGyroAngle() {
+    return gyro.getAngle();
+  }
+
+  public void arcadeDrive(double xSpeed, double zRot) {
+    drive.arcadeDrive(xSpeed, zRot);
+  }
 }
