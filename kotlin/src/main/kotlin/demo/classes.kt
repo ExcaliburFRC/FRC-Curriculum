de81cd08bddd22e31a48834202f15c1f66790eed
elon698 <73128447+elon698@users.noreply.github.com>
2021-05-31 17:59:03 +0300

    finish point class

--- kotlin/src/main/kotlin/demo/classes.kt
+++ kotlin/src/main/kotlin/demo/classes.kt
@@ -1,12 +1,22 @@
 package demo
 
-data class Point(var x: Int, var y: Int) {
-    fun plus(point: Point): Point {
+data class Point(val x: Int, val y: Int) {
+    operator fun plus(point: Point): Point {
 
         val (x1, y1) = this
         val (x2, y2) = point
-
-
+        return Point(x1 + x2, y1 + y2)
     }
 
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println(Point(3, 4) + Point(y = 2, x = 5))
+
+
+        }
+    }
+    init {
+        println(this)
+    }
 }
